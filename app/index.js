8e8da51dfbf8f389f593b6344cedb3bb5b1d7298
furqan-qadri <furqaanqadri@gmail.com>
2024-06-16 20:09:13 +0800

    modified home

--- app/index.js
+++ app/index.js
@@ -3,7 +3,9 @@ import { View, Text } from "react-native";
 const Home = () => {
   return (
     <View>
-      <Text>Home page</Text>
+      <Text>The initial setup for the mobile app is now complete.</Text>
+      <Text>Furqan Qadri has been registered already.</Text>
+      <Text>Please proceed to the next step</Text>
     </View>
   );
 };
